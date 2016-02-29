30d048757432fdd99ee01d6fc10c5e098c8186b9
RodrigoTIburski <rgdesenv@gmail.com>
2016-02-29 20:19:59 -0300

    comitado

--- application/views/header.php
+++ application/views/header.php
@@ -1,4 +1,29 @@
 <div class="container">
     <div id="header">
-      
-  </div>
+
+ <header class="class-down">
+           <div class="bgSizeContain">
+                <h1>Namastê Cosméticos e Perfumaria</h1>
+                <p>Material de cosmética e beleza</p>
+           </div>
+        </header>
+        <div class="img">                                           <!--Classe para add imagem de fundo-->   
+            <div class="container">
+                <div class="row">
+                    <div class="span12">                   
+                        <div class="navbar navbar-inverse">
+                            <div class="navbar-inner">
+                                <ul class="nav"data-spy="affix" data-offset-top="211">
+                                    <li class="active"><a href="#">Home</a></li>                                
+                                    <li><a href="#">About</a></li>                                
+                                    <li><a href="#">Services</a></li>                                
+                                    <li><a href="#">Contact</a></li>                                
+                                </ul>
+                            </div>
+                        </div>
+                   </div>
+                </div>
+            </div> 
+         </div>                
+    
+ </div>
--- application/views/html-header.php
+++ application/views/html-header.php
@@ -9,6 +9,7 @@
         link_tag('assets/css/bootstrap-reponsive.min.css').
         link_tag('assets/css/estilos.css');
         ?>
+      
         <script src="<?php echo base_url('assets/js/jquery-2.1.3.min.js'); ?>"></script>
         <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
         <script src="<?php echo base_url('assets/js/jquery.mask.js') ?>"></script>
